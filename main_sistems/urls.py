af8d3b8a35928794e73dcebc78135a34ce087e35
Sholontla <64672755+Sholontla@users.noreply.github.com>
2021-02-03 16:26:32 -0600

    Purchases

---
+++ main_sistems/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+
+    path('',include(('bases.urls','bases'), namespace='bases')),
+    path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
+    path('fac/', include(('fac.urls', 'fac'), namespace='fac')),
+    path('api/', include(('api.urls', 'api'), namespace='api')),
+    
+    path('admin/', admin.site.urls),
+]
